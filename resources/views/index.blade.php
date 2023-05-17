f6a8546a846c5b7fb9ee13377cb7f6be0dcd0535
Mirabelljoicelaura <mirabelljoice@gmail.com>
2023-05-17 10:34:53 +0700

    update agendapotensisambutan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -106,8 +106,128 @@
 
 
         <!-- ormas -->
-        <h1>Organisasi Masyarakat</h1>
+        {{-- create carousel card autoplay--}}
+        <section class="bg-light" id="organisasi">
+            <div class="container py-5">
+                <div class="p-2 text-center">
+                    <h2 class="text-center blue-title text-capitalize" id="">Organisasi Masyarakat</h2>
+                </div>
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3 m-2">
+                    <div class="col mb-4">
 
+
+
+
+
+        <h1>Organisasi Masyarakat</h1>
+        {{-- agenda --}}
+        <div class="album py-3" id="agenda">
+            <div class="container">
+                <div class="p-2 text-center">
+                    <h2 class="text-center blue-title text-capitalize" id="">Agenda terbaru</h2>
+                </div>
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3 m-2">
+                    <div class="col mb-4">
+                        <div class="card shadow-light">
+                            <img src="https://media.istockphoto.com/id/1164624416/photo/malaysia-hawker-culture-clay-pot-chicken-rice-stock-photo.jpg?b=1&s=170667a&w=0&k=20&c=65L8gxjLg-jpXtSSJVp3wT8c2Z5LL_eMkFTW8DVx9Bk=&auto=format&fit=crop&w=400&h=280&q=80" class="card-img-top object-fit-cover" alt="..." height="200">
+
+                            <div class="card-body">
+                            <h5 class="card-title text-capitalize">Takjil Gratis</h5>
+                            <p class="card-text thumbnail read-toggle" data-id='0'>Takjil gratis adalah sebuah program yang biasanya diadakan pada bulan Ramadan di mana makanan ringan atau minuman manis seperti kolak, kurma, atau air zam-zam dibagikan secara gratis kepada masyarakat yang sedang berpuasa.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="card">
+                        <img src="https://images.unsplash.com/photo-1615647112295-7f6355324a4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&h=275&q=80" class="card-img-top object-fit-cover" alt="..." width="100%" height="200">
+                            <div class="card-body">
+                            <h5 class="card-title text-capitalize">Bazar Murah</h5>
+                            <p class="card-text thumbnail read-toggle" data-id='0'>Bazar murah sembako adalah sebuah acara di mana berbagai sembako (sembilan bahan pokok) seperti beras, minyak goreng, gula, tepung terigu, dan lain-lain dijual dengan harga yang lebih terjangkau atau murah dari harga pasar.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="card">
+                            <img src="https://images.unsplash.com/photo-1631002165139-81c716532830?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&h=275&q=80" class="card-img-top object-fit-cover" alt="..." width="100%" height="200">
+                            <div class="card-body" id="module">
+                            <h5 class="card-title text-capitalize">Perayaan Hari Kemerdekaan RI</h5>
+                                <p class="card-text thumbnail read-toggle" data-id='0'>Perayaan kemerdekaan RI merupakan acara tahunan yang dirayakan pada tanggal 17 Agustus untuk memperingati kemerdekaan Indonesia. Perayaan ini diisi dengan berbagai kegiatan seperti  pawai, lomba, dan pertunjukan seni budaya.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-2 text-center">
+                <a href="#"><button class="btn-more">Lihat Lagi</button></a>
+                </div>
+            </div>
+        </div>
+
+        {{-- potensi --}}
+        <div class="album py-3 pb-5" id="potensi">
+            <div class="container">
+                <div class="p-5 text-center">
+                    <h2 class="text-center blue-title">Potensi UMKM</h2>
+                </div>
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-4 g-4 mx-auto">
+                    <div class="col">
+                        <div class="" style="width: 80%;">
+                            <img src="https://images.unsplash.com/photo-1555126634-323283e090fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" class="potensi-img card-img-top rounded-circle object-fit-cover" alt="..." width="200" height="200">
+                            <div class="card-body mt-3 text-center">
+                            <h5 class="card-title text-capitalize">mie Ayam Pak No</h5>
+                            <p class="card-text mb-1">Mie dengan topping ayam dan kuah segar</p>
+                            <a href="#" class="text-decoration-none fw-bold" id="custom-text-link">beli sekarang
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                                </svg>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="" style="width: 80%;">
+                        <img src="https://images.unsplash.com/photo-1567982047351-76b6f93e38ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="potensi-img card-img-top rounded-circle object-fit-cover" alt="..." width="50%" height="200">
+                        <div class="card-body mt-3 text-center">
+                            <h5 class="card-title text-capitalize">Pecel Ayam Jaya</h5>
+                            <p class="card-text mb-1">Penyetan ayam dengan sambel orek</p>
+                            <a href="#" class="text-decoration-none fw-bold" id="custom-text-link">beli sekarang
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                            </a>
+                        </div>
+                        </div>
+                </div>
+                <div class="col">
+                    <div class="" style="width: 80%;">
+                        <img src="https://images.unsplash.com/photo-1558961363-fa8fdf82db35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" class="potensi-img card-img-top rounded-circle object-fit-cover" alt="..." width="50%" height="200">
+                        <div class="card-body mt-3 text-center">
+                        <h5 class="card-title text-capitalize">Cookie Yahuttt</h5>
+                        <p class="card-text mb-1">Cookie lejatt dengan cokelat meleleh di dalamnya</p>
+                        <a href="#" class="text-decoration-none fw-bold" id="custom-text-link">beli sekarang
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                            </svg>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                <div class="" style="width: 80%;">
+                    <img src="https://images.unsplash.com/photo-1555126634-323283e090fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" class="potensi-img card-img-top rounded-circle object-fit-cover" alt="..." width="50%" height="200">
+                    <div class="card-body mt-3 text-center">
+                        <h5 class="card-title text-capitalize">Kui Cakes</h5>
+                        <p class="card-text mb-1">Aneka kue kering untuk hari raya eid</p>
+                        <a href="#" class="text-decoration-none fw-bold" id="custom-text-link">beli sekarang
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                        </svg>
+                        </a>
+                    </div>
+                    </div>
+            </div>
+                </div>
+            </div>
+        </div>
         <!-- galery start -->
         <section class="container">
             <h1 class="my-4 text-center text-lg-left">Galeri</h1>
